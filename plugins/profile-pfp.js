450e2aac123ee44f359cbd10709fe45dbc72c7dd
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-05-02 17:02:35 -0500

    Update profile-pfp.js

--- plugins/profile-pfp.js
+++ plugins/profile-pfp.js
@@ -3,7 +3,7 @@ let mentionedJid = await m.mentionedJid
 let who = await m.quoted?.sender || mentionedJid?.[0]
 if (!who) return conn.sendMessage(m.chat, { text: '❀ Por favor, menciona al usuario que deseas ver su foto de perfil.' }, { quoted: m })
 let name = await (async () => global.db.data.users[who].name || (async () => { try { const n = await conn.getName(who); return typeof n === 'string' && n.trim() ? n : who.split('@')[0] } catch { return who.split('@')[0] } })())()
-let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
+let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://i.imgur.com/HUyM3Ny.jpeg')
 await m.react('🕒')
 await conn.sendFile(m.chat, pp, 'profile.jpg', `❀ *Foto de perfil de ${name}*`, m)
 await m.react('✔️')
