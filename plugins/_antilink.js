553bb6f9a3077f38c2c77dc1b34814715a79ce92
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-05-04 21:19:36 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,8 +14,10 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
 if (isGroupLink && m.text.includes(linkThisGroup)) return !0
 }
-if (chat.antilink && isGroupLink && !isAdmin && !isROwner && isBotAdmin && m.key.participant !== conn.user.jid) {
+if (chat.antiLink && (isGroupLink || isChannelLink) && !isAdmin && !isROwner && isBotAdmin && m.key.participant !== conn.user.jid) {
+const userName = global.db.data.users[m.key.participant].name || '𝗨𝘀𝘂𝗮𝗿𝗶𝗼'
+const tipoEnlace = isChannelLink ? '𝗰𝗮𝗻𝗮𝗹 𝗱𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽' : '𝗴𝗿𝘂𝗽𝗼 𝗲𝘅𝘁𝗲𝗿𝗻𝗼'
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }})
-await conn.reply(m.chat, `> ꕥ Se ha eliminado a *${global.db.data.users[m.key.participant].name || 'Usuario'}* del grupo por \`Anti-Link\`, no permitimos enlaces de *${isChannelLink ? 'canales' : 'otros grupos'}*.`, null)
+await conn.reply(m.chat, `🚫 *𝗔𝗻𝘁𝗶-𝗟𝗶𝗻𝗸*  @${m.key.participant.split('@')[0]} 𝗳𝘂𝗲 𝗲𝘅𝗽𝘂𝗹𝘀𝗮𝗱𝗼 𝗽𝗼𝗿 𝗲𝗻𝘃𝗶𝗮𝗿 𝘂𝗻 𝗲𝗻𝗹𝗮𝗰𝗲 𝗱𝗲 ${tipoEnlace}.`, null, { ...rcanal, mentions: [m.key.participant] })
 await conn.groupParticipantsUpdate(m.chat, [m.key.participant], 'remove')
 }}}
