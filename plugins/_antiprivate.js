fd3923c1a1d7083eb2924cd8952cb299daea0ef6
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-05-04 21:43:14 -0500

    Update _antiprivate.js

--- plugins/_antiprivate.js
+++ plugins/_antiprivate.js
@@ -3,13 +3,13 @@ if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
 if (m.sender === conn.user?.jid) return
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('code') || m.text.includes('qr')) return !0
-const chat = global.db.data.chats[m.chat]
+const txt = (m.text || '').toUpperCase()
+if (txt.includes('PIEDRA') || txt.includes('PAPEL') || txt.includes('TIJERA') || txt.includes('CODE') || txt.includes('QR')) return !0
 const bot = global.db.data.settings[conn.user.jid] || {}
 if (m.chat === '120363401404146384@newsletter') return !0
 if (bot.antiPrivate && !isROwner) {
-await m.reply(`🚀 Hola @${m.sender.split`@`[0]}, mi dueño a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas a nuestra comunidad.\n\n${community}`, false, {mentions: [m.sender]})
-await this.updateBlockStatus(m.chat, 'block')
+await conn.updateBlockStatus(m.sender, 'block')
+return !0
 }
 return !1
 }
